e03a93cc8d4e7a717bc421a4c71057c2ce74b8f5
Pangyeming <1983156733@qq.com>
2024-12-17 12:15:31 +0800

    chore: Configure markdown-preview,make it can auto-open,auto-combine

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -22,6 +22,8 @@ return {
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_auto_close = 0
     end,
     ft = { "markdown" },
     -- 绑定快捷键映射
